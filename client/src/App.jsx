f5a7daae18c32449faabe2245d0380f81be48902
Pandemonium <jonathancorreaiglesias@gmail.com>
2025-03-04 22:30:36 -0600

    Added the main header and nav/aside elements

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,6 @@
 import { Outlet } from 'react-router-dom';
 import Header from './components/header';
+import Aside from './components/Aside';
 import Footer from './components/Footer';
 import './App.css'
 
@@ -8,6 +9,7 @@ function App() {
   return (
     <>
       <Header />
+      <Aside />
       <main>
         <Outlet />
       </main>
