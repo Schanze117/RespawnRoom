77abc19683c30a1a8dd1c2eb6eb318ef06c6b700
JamixB <bamesb@gmail.com>
2025-03-04 18:16:03 -0800

    Fixed seeding and models

--- server/src/seeds/user-seeds.js
+++ server/src/seeds/user-seeds.js
@@ -1,9 +1,17 @@
 import { User } from '../models/index.js';
 
 export const seedUsers = async () => {
+  try {
     await User.bulkCreate([
-        { userName: 'user1' },
-        { userName: 'user2' },
-        { userName: 'user3' },
-    ], {individualHooks: true});
-}
+      { userName: 'user1' },
+      { userName: 'user2' },
+      { userName: 'user3' },
+    ], {
+      individualHooks: true,
+      returning: true,
+    });
+    console.log('Users have been seeded successfully.');
+  } catch (err) {
+    console.error('Error seeding users:', err);
+  }
+};
