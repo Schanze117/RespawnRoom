63f3ed92abaa83691222bc1e19e5b99352ad1b9b
Pandemonium <jonathancorreaiglesias@gmail.com>
2025-03-06 20:38:30 -0600

    Added the search and discover segments

---
+++ client/src/pages/search.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react';
+import DiscoverForm from '../components/discoverForm';
+import SearchForm from '../components/searchForm';
+
+export default function Search() {
+
+
+    return (
+        <div className="mt-20 sm:ml-55 mr-4 bg-surface-700 border-2 rounded-lg border-tonal-800 height-full">
+            <h1 className="text-3xl font-bold text-light">Search</h1>
+            <SearchForm />
+            <DiscoverForm />
+        </div>
+    )
+}
+
+
+// Seperate search form into a new component
+// Seperate discover form into a new component
