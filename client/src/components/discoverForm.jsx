f9dc82eb208e2a36245c28ba62162a19a750926c
JamixB <bamesb@gmail.com>
2025-03-07 17:53:33 -0800

    Added Sethadsearched to searchform

--- client/src/components/discoverForm.jsx
+++ client/src/components/discoverForm.jsx
@@ -64,7 +64,7 @@ export default function DiscoverForm() {
                     </div>
                 ))
             ) : (
-                <p className="text-light">No results found</p>
+                <p className="text-light"></p>
             )}
         </div>
     </div>
