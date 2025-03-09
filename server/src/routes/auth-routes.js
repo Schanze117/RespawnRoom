002282f4fa8f2841389069d96ffa682e11949cf7
Schanze117 <aaronschanzenbach@gmail.com>
2025-03-09 11:21:09 -0600

    most of the code needed for the jwt authentication, still need to tweak front end

--- server/src/routes/auth-routes.js
+++ server/src/routes/auth-routes.js
@@ -2,6 +2,10 @@ import { Router } from 'express';
 import { User } from '../models/user.js';
 import jwt from 'jsonwebtoken';
 import bcrypt from 'bcrypt';
+
+import dotenv from 'dotenv';
+dotenv.config();
+
 export const login = async (req, res) => {
     const { username, password } = req.body;
     const user = await User.findOne({
