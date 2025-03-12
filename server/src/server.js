fdb1101ace6556edcec5f388267137ff52e91707
Schanze117 <aaronschanzenbach@gmail.com>
2025-03-11 22:38:47 -0600

    debugging for render deploy

--- server/src/server.js
+++ server/src/server.js
@@ -3,6 +3,11 @@ const forceDatabaseRefresh = false;
 import express from 'express';
 import sequelize from './config/connection.js';
 import routes from './routes/index.js';
+import { dirname } from 'node:path';
+import { fileURLToPath } from 'node:url';
+import path from 'node:path';
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -13,7 +18,11 @@ app.use(express.static('../client/dist'));
 
 app.use(express.json());
 app.use(routes);
-
+app.get('*', (req
+  , res) => {
+  res.sendFile(path.join(__dirname, '../../client/dist/index.html'));
+}
+);
 
 sequelize.sync({force: forceDatabaseRefresh}).then(() => {
   app.listen(PORT, () => {
