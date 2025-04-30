8cd0f1499e43289b06db1b1356760c4ad49963b8
JamixB <bamesb@gmail.com>
2025-04-29 19:56:46 -0700

    fixed mutation

--- server/src/middleware/auth.js
+++ server/src/middleware/auth.js
@@ -22,5 +22,5 @@ export const signToken = (username, email, _id) => {
   const payload = { username, email, _id };
   const secretKey = process.env.JWT_SECRET_KEY || '';
 
-  return jwt.sign(payload, secretKey, { expiresIn: '1h' });
+  return jwt.sign(payload, secretKey, { expiresIn: '7d' });
 };
