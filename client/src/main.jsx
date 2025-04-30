c6694c2dc84e6c92773f82a7eece57e25d779274
mahdi <mahdighaleb196@>
2025-04-29 20:05:26 -0700

    loginfixxed

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,6 +1,7 @@
 import ReactDOM from 'react-dom/client';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
-import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
+import { ApolloClient, InMemoryCache, ApolloProvider, createHttpLink } from '@apollo/client';
+import { setContext } from '@apollo/client/link/context';
 import './index.css';
 
 import App from './App.jsx';
@@ -10,9 +11,29 @@ import Login from './pages/loginPage.jsx';
 import Saved from './pages/saved.jsx';
 import Register from './pages/register.jsx';
 
-// Create an Apollo Client instance
+// Create an HTTP link
+const httpLink = createHttpLink({
+  uri: 'http://localhost:3001/graphql',
+  credentials: 'include',
+});
+
+// Create an auth link to include the token in the headers
+const authLink = setContext((_, { headers }) => {
+  // Get the token from localStorage
+  const token = localStorage.getItem('jwtToken');
+  
+  // Return the headers to the context so httpLink can read them
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : '',
+    }
+  };
+});
+
+// Create an Apollo Client instance with the auth link
 const client = new ApolloClient({
-  uri: '/graphql', // Replace with your GraphQL server endpoint
+  link: authLink.concat(httpLink),
   cache: new InMemoryCache(),
 });
 
--- server/src/seeds/videogame-seeds.js
+++ server/src/seeds/videogame-seeds.js
@@ -1,9 +1,67 @@
-import { VideoGame } from "../models/index.js";
+import User from '../models/users.js';
 
 export const seedVideoGames = async () => {
-    await VideoGame.bulkCreate([
-        { name: 'Game1', genre: 'Action', userId: 1 },
-        { name: 'Game2', genre: 'Adventure', userId: 2 },
-        { name: 'Game3', genre: 'RPG', userId: 3 },
-    ]);
-}
+  try {
+    // Get user IDs from the database for associations
+    const users = await User.find({});
+    const userIds = users.map(user => user._id);
+    
+    if (userIds.length === 0) {
+      console.log('No users found to associate with video games.');
+      return;
+    }
+    
+    // Clear any existing savedGames array from users
+    await User.updateMany({}, { $set: { savedGames: [] } });
+    
+    // Sample video game data
+    const sampleGames = [
+      {
+        cover: 'https://images.igdb.com/igdb/image/upload/t_cover_big/co1wyy.jpg',
+        name: 'The Witcher 3: Wild Hunt',
+        genres: ['RPG', 'Adventure'],
+        playerPerspectives: ['Third person'],
+        summary: 'The Witcher 3: Wild Hunt is an action role-playing game set in an open world environment.'
+      },
+      {
+        cover: 'https://images.igdb.com/igdb/image/upload/t_cover_big/co1r7h.jpg',
+        name: 'Red Dead Redemption 2',
+        genres: ['Action', 'Adventure'],
+        playerPerspectives: ['First person', 'Third person'],
+        summary: 'Red Dead Redemption 2 is a western action-adventure game set in an open world environment.'
+      },
+      {
+        cover: 'https://images.igdb.com/igdb/image/upload/t_cover_big/co1rgi.jpg',
+        name: 'God of War',
+        genres: ['Action', 'Adventure'],
+        playerPerspectives: ['Third person'],
+        summary: 'God of War is an action-adventure game developed by Santa Monica Studio.'
+      }
+    ];
+    
+    // Add games to random users
+    for (let i = 0; i < users.length; i++) {
+      // Assign 1-3 random games to each user
+      const numGames = Math.floor(Math.random() * 3) + 1;
+      const userGames = [];
+      
+      for (let j = 0; j < numGames; j++) {
+        // Pick a random game
+        const randomIndex = Math.floor(Math.random() * sampleGames.length);
+        const game = { ...sampleGames[randomIndex], userId: userIds[i] };
+        userGames.push(game);
+      }
+      
+      // Update the user with the games
+      await User.findByIdAndUpdate(
+        userIds[i],
+        { $push: { savedGames: { $each: userGames } } },
+        { new: true }
+      );
+    }
+    
+    console.log('Video games have been seeded successfully.');
+  } catch (err) {
+    console.error('Error seeding video games:', err);
+  }
+};
